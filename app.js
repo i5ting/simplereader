9cddf3bd2cbdaa127c74fa36016548b19ee864a8
i5ting <i5ting@126.com>
2016-07-10 13:43:28 +0800

    lazycommmit, please ignore

---
+++ app.js
@@ -0,0 +1,31 @@
+var serve = require('koa-static');
+var Koa = require('koa');
+var app = new Koa();
+
+var favicon = require('koa-favicon');
+var compress = require('koa-compress')
+var conditional = require('koa-conditional-get');
+var etag = require('koa-etag');
+
+app.use(compress({
+  filter: function (content_type) {
+    return /text/i.test(content_type)
+  },
+  threshold: 2048,
+  flush: require('zlib').Z_SYNC_FLUSH
+}))
+
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// etag works together with conditional-get
+app.use(conditional());
+app.use(etag());
+
+
+// or use absolute paths
+app.use(serve(__dirname + '/dist'));
+
+app.listen(9090);
+
+console.log('listening on port 9090');
